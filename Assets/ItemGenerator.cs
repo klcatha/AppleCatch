fd3da49c02de8f0ab2b635c3365ba2e14556c5ae
klcatha <klcatha0518@gmail.com>
2018-10-28 11:51:15 +0900

    folling item type random

--- Assets/ItemGenerator.cs
+++ Assets/ItemGenerator.cs
@@ -7,6 +7,7 @@ public class ItemGenerator : MonoBehaviour {
 	public GameObject bombPrefab;
 	float span = 1.0f;
 	float delta = 0.0f;
+	int ratio = 2;
 
 	// Use this for initialization
 	void Start () {
@@ -19,7 +20,15 @@ public class ItemGenerator : MonoBehaviour {
 		if(this.delta > this.span)
 		{
 			this.delta = 0.0f;
-			GameObject item = Instantiate(applePrefab) as GameObject;
+			GameObject item;
+			int dice = Random.Range(1, 11);
+			if(dice <= this.ratio)
+			{
+				item = Instantiate(bombPrefab) as GameObject;
+			} else {
+				item = Instantiate(applePrefab) as GameObject;
+
+			}
 			float x = Random.Range(-1, 2);
 			float z = Random.Range(-1, 2);
 			item.transform.position = new Vector3(x, 4, z);
