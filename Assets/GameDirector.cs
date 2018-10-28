9d6d23ba0c5635cad72cf048d9536b8d1a993920
klcatha <klcatha0518@gmail.com>
2018-10-28 12:22:23 +0900

    point increase/decrease

--- Assets/GameDirector.cs
+++ Assets/GameDirector.cs
@@ -6,11 +6,24 @@ using UnityEngine.UI;
 public class GameDirector : MonoBehaviour {
 
 	GameObject timerText;
+	GameObject pointText;
 	float time = 60.0f;
+	int point = 0;
+
+	public void GetApple()
+	{
+		this.point += 100;
+	}
+
+	public void GetBomb()
+	{
+		this.point /= 2;
+	}
 
 	// Use this for initialization
 	void Start () {
 		this.timerText = GameObject.Find("Time");
+		this.pointText = GameObject.Find("Point");
 	}
 	
 	// Update is called once per frame
@@ -18,6 +31,8 @@ public class GameDirector : MonoBehaviour {
 		this.time -= Time.deltaTime;
 		this.timerText.GetComponent<Text>().text =
 			this.time.ToString("F1");
+		this.pointText.GetComponent<Text>().text =
+			this.point.ToString() + " Point";
 		
 	}
 }
